9bcd0e5252dcf08032ba2241f26009bfa2e56d48
garytto <76801853+garytto@users.noreply.github.com>
2023-01-19 13:34:17 +0900

    분류

--- python/수업실습/0118_4회차_김건희.py
+++ python/수업실습/0118_4회차_김건희.py
@@ -1,18 +1,18 @@
 import sys
 input = sys.stdin.readline
 # 9498 시험성적
-a = int(input())
+# a = int(input())
 
-if a <= 100 and a > 89:
-    print('A')
-elif a > 79: ## elif > 사용 해보기. #
-    print('B')
-elif a > 69:
-    print('C')
-elif a > 59:
-    print('D')
-else:
-    print('F')
+# if a <= 100 and a > 89:
+#     print('A')
+# elif a > 79: ## elif > 사용 해보기. #
+#     print('B')
+# elif a > 69:
+#     print('C')
+# elif a > 59:
+#     print('D')
+# else:
+#     print('F')
 
 # 9093 단어 뒤집기
 # T = int(input())
@@ -30,15 +30,16 @@ else:
 #     # print(i) # 정상적으로 순서에 맞춰 진행되는지 확인. 위에 range(0, len(string), 10) <- i가 10씩 되는지 확인용
 #     print(string[i:endline])
 
+
 # 2947 나무조각
-numbers = list(map(int, input().split()))
-answer = [1, 2, 3, 4, 5]
+# numbers = list(map(int, input().split()))
+# answer = [1, 2, 3, 4, 5]
 
-while True:
-    for i in range(len(numbers)-1):
-        if numbers[i] > numbers[i+1]:
-            numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
-            print(" ".join(map(str, numbers))) # 이 문제는 정답을 찾는 과정을 출력을 같이 해줘야하는 문제이기에..
+# while True:
+#     for i in range(len(numbers)-1):
+#         if numbers[i] > numbers[i+1]:
+#             numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+#             print(" ".join(map(str, numbers))) # 이 문제는 정답을 찾는 과정을 출력을 같이 해줘야하는 문제이기에..
 
-    if numbers == answer:
-        break
+#     if numbers == answer:
+#         break
